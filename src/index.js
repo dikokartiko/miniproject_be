01adeb63f5f08685729fd4872842ba3ef80d253d
dikokartiko <dikowebdev@gmail.com>
2023-07-27 14:54:38 +0700

    cleaning comments

--- src/index.js
+++ src/index.js
@@ -8,14 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Sync models with database
 sequelize.sync({ alter: true });
 
-// Set up routes
 app.use("/api/users", userRoutes);
 app.use(blogRoutes);
 
-// Start server
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
